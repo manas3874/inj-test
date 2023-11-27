6f4c1cd6e94b60311674f0ba645017d59e65c7f3
Manas Tripathi <TRIPATHIMANAS98@GMAIL.COM>
2023-11-27 22:27:49 +0530

    initial commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,151 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import { useState } from "react";
+import {
+  MsgSend,
+  BaseAccount,
+  DEFAULT_STD_FEE,
+  ChainRestAuthApi,
+  createTransaction,
+  CosmosTxV1Beta1Tx,
+  ChainRestTendermintApi,
+  getTxRawFromTxRawOrDirectSignResponse,
+  TxRestClient,
+} from "@injectivelabs/sdk-ts";
+import {
+  // DEFAULT_STD_FEE,
+  BigNumberInBase,
+  DEFAULT_BLOCK_TIMEOUT_HEIGHT,
+} from "@injectivelabs/utils";
+import { ChainId } from "@injectivelabs/ts-types";
+import { Network, getNetworkEndpoints } from "@injectivelabs/networks";
+const inter = Inter({ subsets: ["latin"] });
+let testWalletAddr = "inj17xxadj7e9ermxnq7jl5t2zxu5pknhahac8ma8e";
 
 export default function Home() {
+  const [publicAddress, setPublicAddress] = useState("");
+  async function getKeplr() {
+    if (!window.keplr) {
+      alert("Please install Keplr extension");
+      return;
+    }
+    await window.keplr.enable("injective-888"); // Example: Enable the Cosmos Hub chain
+    const keplr = window.keplr;
+    return keplr;
+  }
+  async function connectWallet() {
+    const keplr = await getKeplr();
+    if (!keplr) return;
+
+    const chainId = "injective-888";
+
+    const offlineSigner = keplr.getOfflineSigner(chainId);
+    const accounts = await offlineSigner.getAccounts();
+    setPublicAddress(accounts[0].address);
+    console.log({ keplr, accounts });
+    return { keplr, accounts };
+  }
+  async function createAndSignTransaction(
+    injectiveAddress: string,
+    chainId: string,
+    restEndpoint: string,
+    amount: { denom: string; amount: string; } | { denom: string; amount: string; }[]
+  ) {
+    const getKeplr = async (chainId: string) => {
+      await window.keplr.enable(chainId);
+      const offlineSigner = window.keplr.getOfflineSigner(chainId);
+      const accounts = await offlineSigner.getAccounts();
+      const key = await window.keplr.getKey(chainId);
+      return { offlineSigner, accounts, key };
+    };
+
+    const broadcastTx = async (chainId, txRaw) => {
+      const keplr = await getKeplr(chainId);
+      const result = await keplr.sendTx(
+        chainId,
+        CosmosTxV1Beta1Tx.TxRaw.encode(txRaw).finish(),
+        BroadcastMode.Sync
+      );
+
+      if (!result || result.length === 0) {
+        throw new TransactionException(
+          new Error("Transaction failed to be broadcasted"),
+          { contextModule: "Keplr" }
+        );
+      }
+
+      return Buffer.from(result).toString("hex");
+    };
+
+    // Account Details
+    const chainRestAuthApi = new ChainRestAuthApi(restEndpoint);
+    const accountDetailsResponse = await chainRestAuthApi.fetchAccount(
+      injectiveAddress
+    );
+    const baseAccount = BaseAccount.fromRestApi(accountDetailsResponse);
+    const accountDetails = baseAccount.toAccountDetails();
+
+    // Block Details
+    const chainRestTendermintApi = new ChainRestTendermintApi(restEndpoint);
+    const latestBlock = await chainRestTendermintApi.fetchLatestBlock();
+    const latestHeight = latestBlock.header.height;
+    const timeoutHeight = new BigNumberInBase(latestHeight).plus(
+      DEFAULT_BLOCK_TIMEOUT_HEIGHT
+    );
+
+    // Preparing the transaction
+    const msg = MsgSend.fromJSON({
+      amount,
+      srcInjectiveAddress: publicAddress,
+      dstInjectiveAddress: injectiveAddress,
+    });
+
+    // Get the PubKey of the Signer from the Wallet/Private Key
+    const { offlineSigner } = await getKeplr(chainId);
+    const pubKey = await window.keplr.getKey(chainId); // Assuming getPubKey() is defined elsewhere
+    console.log("pubKey", pubKey);
+    // Prepare the Transaction
+    const { txRaw: txRawToSign, signDoc } = createTransaction({
+      pubKey,
+      chainId,
+      fee: DEFAULT_STD_FEE,
+      message: [msg], // Assuming msg is the message to be sent
+      sequence: baseAccount.sequence,
+      timeoutHeight: timeoutHeight.toNumber(),
+      accountNumber: baseAccount.accountNumber,
+    });
+
+    const directSignResponse = await offlineSigner.signDirect(
+      injectiveAddress,
+      signDoc
+    );
+    const txRaw = getTxRawFromTxRawOrDirectSignResponse(directSignResponse); // Assuming this function is defined elsewhere
+
+    const txHash = await broadcastTx(chainId, txRaw);
+    const response = await new TxRestClient(restEndpoint).fetchTxPoll(txHash);
+
+    return response;
+  }
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <button onClick={connectWallet}>Connect wallet</button>
+      <span>{publicAddress}</span>
+      <button
+        onClick={() => {
+          createAndSignTransaction(
+            testWalletAddr,
+            "injective-888",
+            getNetworkEndpoints(Network.Testnet).rest,
+            { amount: "1", denom: "inj" }
+          )
+            .then((response) => console.log("Transaction Response:", response))
+            .catch((error) => console.error("Error:", error));
+        }}
+      >
+        Transact
+      </button>
     </main>
-  )
+  );
 }
