9f7e240288c2138b86af575733d72f10e6cab285
Manas Tripathi <TRIPATHIMANAS98@GMAIL.COM>
2023-12-06 21:12:44 +0530

    trying walletstrategy

---
+++ src/pages/test-2.tsx
@@ -0,0 +1,72 @@
+import {
+  MsgBroadcaster,
+  Wallet,
+  WalletStrategy,
+} from "@injectivelabs/wallet-ts";
+import { MsgSend } from "@injectivelabs/sdk-ts";
+import { BigNumberInBase } from "@injectivelabs/utils";
+import { Network, getNetworkEndpoints } from "@injectivelabs/networks";
+import { ChainId } from "@injectivelabs/ts-types";
+
+import React from "react";
+export const walletStrategy = new WalletStrategy({
+  chainId: ChainId.Testnet,
+  wallet: Wallet.Keplr,
+});
+export const msgBroadcastClient = new MsgBroadcaster({
+  walletStrategy /* instantiated wallet strategy */,
+  network: Network.Testnet,
+});
+// (
+//   // Usage Example
+//   async () => {
+//     const signer = "inj1udj57jjtd4vmp9l99v29wu75xshumvqz5vsk0c";
+
+//     const msg = MsgSend.fromJSON({
+//       amount: {
+//         denom: "inj",
+//         amount: new BigNumberInBase(0.01).toWei().toFixed(),
+//       },
+//       srcInjectiveAddress: signer,
+//       dstInjectiveAddress: "inj1...",
+//     });
+
+//     // Prepare + Sign + Broadcast the transaction using the Wallet Strategy
+//     await msgBroadcastClient.broadcast({
+//       injectiveAddress: signer,
+//       msgs: msg,
+//     });
+//   }
+// )();
+
+function Test() {
+  console.log("walletStrategy", walletStrategy);
+  async function run() {
+    const signer = "inj1udj57jjtd4vmp9l99v29wu75xshumvqz5vsk0c";
+
+    const msg = MsgSend.fromJSON({
+      amount: {
+        denom: "inj",
+        amount: new BigNumberInBase(0.01).toWei().toFixed(),
+      },
+      srcInjectiveAddress: signer,
+      dstInjectiveAddress: "inj17xxadj7e9ermxnq7jl5t2zxu5pknhahac8ma8e",
+    });
+
+    // Prepare + Sign + Broadcast the transaction using the Wallet Strategy
+    await msgBroadcastClient.broadcast({
+      injectiveAddress: signer,
+      msgs: [msg],
+    });
+  }
+  return (
+    <div>
+      Test{" "}
+      <button onClick={run} className="bg-gray-200 text-black">
+        Run tx
+      </button>
+    </div>
+  );
+}
+
+export default Test;
--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,5 +1,5 @@
 // Filename: InjectiveTxPage.tsx
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import {
   MsgSend,
   BaseAccount,
@@ -24,111 +24,106 @@ import { Network, getNetworkEndpoints } from "@injectivelabs/networks";
 const InjectiveTxPage = () => {
   const [txHash, setTxHash] = useState("");
 
-  useEffect(() => {
-    const executeTransaction = async () => {
-      // Define your functions and variables here
-      const getKeplr = async (chainId) => {
-        await window.keplr.enable(chainId);
+  const executeTransaction = async () => {
+    // Define your functions and variables here
+    const getKeplr = async (chainId) => {
+      await window.keplr.enable(chainId);
 
-        const offlineSigner = window.keplr.getOfflineSigner(chainId);
-        const accounts = await offlineSigner.getAccounts();
-        const key = await window.keplr.getKey(chainId);
+      const offlineSigner = window.keplr.getOfflineSigner(chainId);
+      const accounts = await offlineSigner.getAccounts();
+      const key = await window.keplr.getKey(chainId);
 
-        return { offlineSigner, accounts, key };
-      };
-
-      const broadcastTx = async (chainId, txRaw) => {
-        const result = await window.keplr.sendTx(
-          chainId,
-          CosmosTxV1Beta1Tx.TxRaw.encode(txRaw).finish(),
-          'sync'
-        );
-
-        if (!result || result.length === 0) {
-          throw new TransactionException(
-            new Error("Transaction failed to be broadcasted"),
-            { contextModule: "Keplr" }
-          );
-        }
-
-        return Buffer.from(result).toString("hex");
-      };
-      const chainId = ChainId.Testnet
-      const {key, offlineSigner} = await getKeplr(chainId)
-      const pubKey = Buffer.from(key.pubKey).toString("base64");
-      const injectiveAddress = key.bech32Address;
-      const restEndpoint = getNetworkEndpoints(
-        Network.Testnet
-      ).rest; /* getNetworkEndpoints(Network.Mainnet).rest */
-      const amount = {
-        amount: new BigNumberInBase(0.01).toWei().toFixed(),
-        denom: "inj",
-      };
-
-      /** Account Details **/
-      const chainRestAuthApi = new ChainRestAuthApi(restEndpoint);
-      const accountDetailsResponse = await chainRestAuthApi.fetchAccount(
-        injectiveAddress
-      );
-      const baseAccount = BaseAccount.fromRestApi(accountDetailsResponse);
-      const accountDetails = baseAccount.toAccountDetails();
-
-      /** Block Details */
-      const chainRestTendermintApi = new ChainRestTendermintApi(restEndpoint);
-      const latestBlock = await chainRestTendermintApi.fetchLatestBlock();
-      const latestHeight = latestBlock.header.height;
-      const timeoutHeight = new BigNumberInBase(latestHeight).plus(
-        DEFAULT_BLOCK_TIMEOUT_HEIGHT
-      );
-
-      /** Preparing the transaction */
-      const msg = MsgSend.fromJSON({
-        amount,
-        srcInjectiveAddress: injectiveAddress,
-        dstInjectiveAddress: "inj17xxadj7e9ermxnq7jl5t2zxu5pknhahac8ma8e",
-      });
-
-      try {
-        const { signDoc } = createTransaction({
-          pubKey,
-          chainId,
-          fee: DEFAULT_STD_FEE,
-          signMode: SIGN_DIRECT,
-          message: [msg],
-          sequence: baseAccount.sequence,
-          timeoutHeight: timeoutHeight.toNumber(),
-          accountNumber: baseAccount.accountNumber,
-        });
-
-        const directSignResponse = await offlineSigner.signDirect(
-          injectiveAddress,
-          createCosmosSignDocFromSignDoc(signDoc),
-        )
-        const txRaw = getTxRawFromTxRawOrDirectSignResponse(directSignResponse)
-        const txHash = await broadcastTx(ChainId.Testnet, txRaw);
-        console.log({txHash})
-        const response = await new TxRestClient(restEndpoint).fetchTxPoll(
-          txHash
-        );
-        console.log({response})
-        setTxHash(txHash);
-      } catch (error) {
-        console.error("Error executing transaction:", error);
-      }
+      return { offlineSigner, accounts, key };
     };
 
-    if (typeof window !== "undefined") {
-      // Ensures this code is not executed on the server side
-      executeTransaction();
+    const broadcastTx = async (chainId, txRaw) => {
+      const result = await window.keplr.sendTx(
+        chainId,
+        CosmosTxV1Beta1Tx.TxRaw.encode(txRaw).finish(),
+        'sync'
+      );
+
+      if (!result || result.length === 0) {
+        throw new TransactionException(
+          new Error("Transaction failed to be broadcasted"),
+          { contextModule: "Keplr" }
+        );
+      }
+
+      return Buffer.from(result).toString("hex");
+    };
+    const chainId = ChainId.Testnet
+    const {key, offlineSigner} = await getKeplr(chainId)
+    const pubKey = Buffer.from(key.pubKey).toString("base64");
+    const injectiveAddress = key.bech32Address;
+    const restEndpoint = getNetworkEndpoints(
+      Network.Testnet
+    ).rest; /* getNetworkEndpoints(Network.Mainnet).rest */
+    const amount = {
+      amount: new BigNumberInBase(0.01).toWei().toFixed(),
+      denom: "inj",
+    };
+
+    /** Account Details **/
+    const chainRestAuthApi = new ChainRestAuthApi(restEndpoint);
+    const accountDetailsResponse = await chainRestAuthApi.fetchAccount(
+      injectiveAddress
+    );
+    const baseAccount = BaseAccount.fromRestApi(accountDetailsResponse);
+    const accountDetails = baseAccount.toAccountDetails();
+
+    /** Block Details */
+    const chainRestTendermintApi = new ChainRestTendermintApi(restEndpoint);
+    const latestBlock = await chainRestTendermintApi.fetchLatestBlock();
+    const latestHeight = latestBlock.header.height;
+    const timeoutHeight = new BigNumberInBase(latestHeight).plus(
+      DEFAULT_BLOCK_TIMEOUT_HEIGHT
+    );
+
+    /** Preparing the transaction */
+    const msg = MsgSend.fromJSON({
+      amount,
+      srcInjectiveAddress: injectiveAddress,
+      dstInjectiveAddress: "inj17xxadj7e9ermxnq7jl5t2zxu5pknhahac8ma8e",
+    });
+
+    try {
+      const { signDoc } = createTransaction({
+        pubKey,
+        chainId,
+        fee: DEFAULT_STD_FEE,
+        signMode: SIGN_DIRECT,
+        message: [msg],
+        sequence: baseAccount.sequence,
+        timeoutHeight: timeoutHeight.toNumber(),
+        accountNumber: baseAccount.accountNumber,
+      });
+
+      const directSignResponse = await offlineSigner.signDirect(
+        injectiveAddress,
+        createCosmosSignDocFromSignDoc(signDoc),
+      )
+      const txRaw = getTxRawFromTxRawOrDirectSignResponse(directSignResponse)
+      const txHash = await broadcastTx(ChainId.Testnet, txRaw);
+      console.log({txHash})
+      const response = await new TxRestClient(restEndpoint).fetchTxPoll(
+        txHash
+      );
+      console.log({response})
+      setTxHash(txHash);
+    } catch (error) {
+      console.error("Error executing transaction:", error);
     }
-  }, []);
+  };
 
   return (
     <div>
       <h1>Injective Transaction</h1>
+      <button onClick={executeTransaction}>Transact</button>
       {txHash && <p>Transaction Hash: {txHash}</p>}
     </div>
   );
 };
 
 export default InjectiveTxPage;
+``
